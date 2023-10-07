45400bc5ca20cd7dc6dc32fcf0d9ea6b4284fa39
Nathanael Rybin <nate.rybin@gmail.com>
2023-10-07 14:51:53 -0600

    Updated password.py with new iteration

--- password.py
+++ password.py
@@ -1,13 +1,22 @@
 import random
 
-n = input('What length do you want your password to be? \n')
+while True:
+    try:
+        n = int(input('What length do you want your password to be? \n'))
+        if n > 0:
+            break
+        else:
+            print("Invalid input. Input must be positive.")
+    except:
+        print("Invalid input. Input must be an integer.")
 
-upperCaseLetter = chr(random.randint(65, 90))
-lowerCaseLetter = chr(random.randint(97,122))
-numberChar = chr(random.randint(48,57))
-specialChar1 = chr(random.randint(33,47))
-specialChar2 = chr(random.randint(58,64))
 
+for i in range(n):
+    upperCaseLetter = chr(random.randint(65, 90))
+    lowerCaseLetter = chr(random.randint(97,122))
+    numberChar = chr(random.randint(48,57))
+    specialChar1 = chr(random.randint(33,47))
+    specialChar2 = chr(random.randint(58,64))
 
-print(upperCaseLetter, lowerCaseLetter, numberChar, specialChar1, specialChar2)
+    print(upperCaseLetter, lowerCaseLetter, numberChar, specialChar1, specialChar2)
 
